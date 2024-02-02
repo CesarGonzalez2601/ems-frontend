513cd2fc0054d41da6127472aefdb0020a487da6
CesarGonzalez2601 <cesareliasrodas26@gmail.com.com>
2024-02-01 23:35:50 -0600

    Agregue y descargue las librerias de rutas para usar los demas endpoints

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,20 @@ import './App.css'
 import HeaderComponent from './components/HeaderComponent'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
 import FooterComponent from './components/FooterComponent'
-
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 function App() {
   
   return (
     <>
+    <BrowserRouter>
     <HeaderComponent/>
-    <ListEmployeeComponent/>
-    <FooterComponent/> </>
+    <Routes>
+      <Route path='/' element={<ListEmployeeComponent/>}></Route>
+      <Route path='/employees' element = { <ListEmployeeComponent/>}></Route>
+      </Routes>
+    <FooterComponent/> 
+    </BrowserRouter>
+    </>
   )
 }
 
